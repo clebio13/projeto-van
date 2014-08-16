a3b3797fbb50ced04645abf07146506d47c14ff5
josias fontes <josiasfontesjp@hotmail.com>
2014-08-16 19:27:12 -0300

    Update tabelaMotorista.php

--- tabelaMotorista.php
+++ tabelaMotorista.php
@@ -19,12 +19,12 @@
 				<td >ID</td>
 				<td >Nome Usuario</td>
 				<td >Senha</td>
-				<td >Nome</td>
+				<td >Email</td>
+				<td >Nome Completo</td>
 				<td >Endereco</td>
 				<td>Telefone1</td>
 				<td>Telefone2</td>
 				<td>CNH</td>
-				<td>Tipo</td>
 			</tr>
 		
 <?php
@@ -37,32 +37,32 @@
 		header("LOCATION:index.html?msg=SESSAO_FINALIZADA");
 	}
 	
-	$strSQL = "SELECT * FROM usuario WHERE TIPO = 'motorista'";
+	$strSQL = "SELECT * FROM MOTORISTA";
 
 	$rs = mysql_query($strSQL);	
 	// Cada linha vai para um array ($row) usando mysql_fetch_array
 	while($row = mysql_fetch_array($rs,MYSQL_BOTH)) {
-		$idPassageiro = $row['ID_USUARIO'];
-		$Nomeusuario = $row['NOME_USUARIO'];
+		$idMotorista = $row['ID_MOTORISTA'];
+		$nomeusuario = $row['NOME_USUARIO'];
 		$senha = $row['SENHA'];
-		$nome = $row['NOME'];
+		$email = $row['EMAIL'];
+		$nomeCompleto = $row['NOME_COMPLETO'];
 		$endereco = $row['ENDERECO'];
 		$telefone1 = $row['TELEFONE1'];
 		$telefone2 = $row['TELEFONE2'];
 		$cnh = $row['CNH'];
-		$tipo = $row['TIPO'];
 
 	  	echo "
 		<tr> 
-	    <td>$idPassageiro</td>
-	    <td>$Nomeusuario</td>
+	    <td>$idMotorista</td>
+	    <td>$nomeusuario</td>
 	    <td>$senha</td>
-	    <td>$nome</td>
+	    <td>$email</td>
+	    <td>$nomeCompleto</td>
 	    <td>$endereco</td>
 	  	<td>$telefone1</td>
 	  	<td>$telefone2</td>
 	  	<td>$cnh</td>
-	  	<td>$tipo</td>
 	  	</tr>";	
 	}
 	// Encerra a conexão
