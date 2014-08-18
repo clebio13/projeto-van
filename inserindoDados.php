94fc4c8de7901470d8d9086be9e37cb06777c492
jarquison <jacksonlira.rs@gmail.com>
2014-08-18 19:33:58 -0300

    Prenchendo fomulario
    Quando estar cadstrando motorista, verfica se nome de usuário já existe,
    caso sim, permanece na página de cadastro.

--- inserindoDados.php
+++ inserindoDados.php
@@ -21,7 +21,7 @@ $cnh = $_POST['CNH'];
 
 	if(mysql_num_rows($busca2) == 1){
 			
-		echo "<script>alert('Nome de Usuario ja Existe. Tente Novamente!');top.location.href='index.html';</script>";
+		echo "<script>alert('Nome de Usuario ja Existe. Tente Novamente!');top.location.href='fomularioMotorista.html';</script>";
 
 	}else{
 
